491501f72d8a85f27d29137eeb181c7ed554613f
Ivan Saveliev <vanjusha-vlad@yandex.ru>
2023-01-07 01:36:32 +0300

    Test with memes

--- bot.py
+++ bot.py
@@ -1,33 +1,32 @@
 import discord
-import random
 from discord.ext import commands
-import config
-import requests
-import json
+from config import settings as set
+from config import urls
 
 def run_bot():
     intents = discord.Intents.default() # бот имеет полные права доступа
     intents.message_content = True
-    bot = commands.Bot(command_prefix = config.prefix, intents = intents)
+    bot = commands.Bot(command_prefix = set['prefix'], intents = intents)
 
     @bot.command()
-    async def rand(ctx, *arg):
-        await ctx.reply(random.randint(0,100))
+    async def mamamia(ctx):
+        embed = discord.Embed(color = 0xff2800)
+        embed.set_image(url = urls['mamamia'])
+        await ctx.send(embed = embed)
+
+    # @bot.command()
+    # async def bolid(ctx):
+    #     embed = discord.Embed(color = 0xff2800)
+    #     embed.set_image(url = urls['bolid'])
+    #     await ctx.send(embed = embed)
 
     @bot.command()
-    async def hello(ctx):
-        author = ctx.message.author
-        await ctx.send(f'Hello, {author.mention}!')
+    async def bolid(ctx):
+        await new_command(ctx,0xff2800,'bolid')
 
-    @bot.command()
-    async def fox(ctx):
-        response = requests.get('https://some-random-api.ml/img/fox') # Get-запрос
-        print(response.text)
-        json_data = json.loads(response.text) # Извлекаем JSON
-        # print(json_data)
-
-        embed = discord.Embed(color = 0xff9900, title = 'Random Fox') # Создание Embed'a
-        embed.set_image(url = json_data['link']) # Устанавливаем картинку Embed'a
-        await ctx.send(embed = embed) # Отправляем Embed
+    async def new_command(ctx,color,url):
+        embed = discord.Embed(color = color)
+        embed.set_image(url = urls[url])
+        await ctx.send(embed = embed)
     
-    bot.run(config.token)
+    bot.run(set['token'])
--- config.py
+++ config.py
@@ -1,4 +1,8 @@
-prefix = '!'
-
-token = 'MTA2MDY0Mzc0MzMzNTcxNDg3Ng.GK8XGn.auRr9_XJgztStR_aEG20eRA3VVTETLwdawsbyQ'
-
+settings = {
+    'prefix': '!',
+    'token': 'MTA2MDY0Mzc0MzMzNTcxNDg3Ng.G6AqP2.jq1PFVEAX2FlkSo-Hn5LlvCDq2Ehdm0yNDyCNE'
+}
+urls = {
+    'mamamia': 'https://cdn.discordapp.com/attachments/698200479217483860/1041419679903784960/telegram-cloud-photo-size-2-5442857354250011359-x.jpg',
+    'bolid': 'https://cdn.discordapp.com/attachments/698200479217483860/1041414868420534352/PgCb6Si_c4t5HgY7Jn_KtexcWJtGR8TbI9vhpFEwIauGDJ1Xrftrylfl6bbl7qHh-wwUn7SUttIWGZuAT08ArR7m.jpg'
+}
