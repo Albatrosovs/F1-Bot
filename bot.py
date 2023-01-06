40e59edf8e1010b7519b087985d073261c181239
Ivan Saveliev <vanjusha-vlad@yandex.ru>
2023-01-06 22:37:32 +0300

    Random foxes test

--- bot.py
+++ bot.py
@@ -1,14 +1,33 @@
 import discord
 import random
 from discord.ext import commands
+import config
+import requests
+import json
 
 def run_bot():
-    intents = discord.Intents.default()
+    intents = discord.Intents.default() # бот имеет полные права доступа
     intents.message_content = True
-    bot = commands.Bot(command_prefix='!', intents=intents)
+    bot = commands.Bot(command_prefix = config.prefix, intents = intents)
 
     @bot.command()
     async def rand(ctx, *arg):
-     await ctx.reply(random.randint(0,100))
+        await ctx.reply(random.randint(0,100))
+
+    @bot.command()
+    async def hello(ctx):
+        author = ctx.message.author
+        await ctx.send(f'Hello, {author.mention}!')
+
+    @bot.command()
+    async def fox(ctx):
+        response = requests.get('https://some-random-api.ml/img/fox') # Get-запрос
+        print(response.text)
+        json_data = json.loads(response.text) # Извлекаем JSON
+        # print(json_data)
+
+        embed = discord.Embed(color = 0xff9900, title = 'Random Fox') # Создание Embed'a
+        embed.set_image(url = json_data['link']) # Устанавливаем картинку Embed'a
+        await ctx.send(embed = embed) # Отправляем Embed
     
-    bot.run('MTA2MDY0Mzc0MzMzNTcxNDg3Ng.GGTpdi.Sq8YzJ7kMjWqxMcoZrCYqEFakZ0917tgpMAQac')
+    bot.run(config.token)
