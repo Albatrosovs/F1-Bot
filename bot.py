c5d252c6cd251401e711ff045465a901090f46fa
Ivan Saveliev <vanjusha-vlad@yandex.ru>
2023-01-07 16:44:49 +0300

    Added some memes

--- bot.py
+++ bot.py
@@ -2,30 +2,79 @@ import discord
 from discord.ext import commands
 from config import settings as set
 from config import urls
+from config import colors
 
 def run_bot():
-    intents = discord.Intents.default() # бот имеет полные права доступа
+    intents = discord.Intents.default()
     intents.message_content = True
     bot = commands.Bot(command_prefix = set['prefix'], intents = intents)
 
     @bot.command()
     async def mamamia(ctx):
-        embed = discord.Embed(color = 0xff2800)
-        embed.set_image(url = urls['mamamia'])
-        await ctx.send(embed = embed)
-
-    # @bot.command()
-    # async def bolid(ctx):
-    #     embed = discord.Embed(color = 0xff2800)
-    #     embed.set_image(url = urls['bolid'])
-    #     await ctx.send(embed = embed)
+        await new_command(ctx,'ferrari','mamamia')
 
     @bot.command()
     async def bolid(ctx):
-        await new_command(ctx,0xff2800,'bolid')
+        await new_command(ctx,'ferrari','bolid')
+
+    @bot.command()
+    async def horner(ctx):
+        await new_command(ctx,'redbull','horner')
+
+    @bot.command()
+    async def auf(ctx):
+        await new_command(ctx,'mercedes','auf')
+
+    @bot.command()
+    async def lechair(ctx):
+        await new_command(ctx,'ferrari','lechair')
+
+    @bot.command()
+    async def leave(ctx):
+        await new_command(ctx,'mercedes','leave')
+
+    @bot.command()
+    async def mazepin(ctx):
+        await new_command(ctx,'haas','mazepin')
+
+    @bot.command()
+    async def classic(ctx):
+        await new_command(ctx,'ferrari','classic_ferrari')
+
+    @bot.command()
+    async def kind_gasly(ctx):
+        await new_command(ctx,'alpine','kind_gasly')
+
+    @bot.command()
+    async def piston(ctx):
+        await new_command(ctx,'ferrari','piston')
+
+    @bot.command()
+    async def genshin(ctx):
+        await new_command(ctx,'f1','genshin')
+
+    @bot.command()
+    async def max(ctx):
+        await new_command(ctx,'mercedes','max')
+
+    @bot.command()
+    async def goatifi(ctx):
+        await new_command(ctx,'williams','goatifi')
+
+    @bot.command()
+    async def beer(ctx):
+        await new_command(ctx,'alfaromeo','beer')
+
+    @bot.command()
+    async def clowns(ctx):
+        await new_command(ctx,'ferrari','clowns')
+
+    @bot.command()
+    async def strategy(ctx):
+        await new_command(ctx,'ferrari','strategy')
 
     async def new_command(ctx,color,url):
-        embed = discord.Embed(color = color)
+        embed = discord.Embed(color = colors[color])
         embed.set_image(url = urls[url])
         await ctx.send(embed = embed)
     
--- config.py
+++ config.py
@@ -1,8 +1,35 @@
 settings = {
     'prefix': '!',
-    'token': 'MTA2MDY0Mzc0MzMzNTcxNDg3Ng.G6AqP2.jq1PFVEAX2FlkSo-Hn5LlvCDq2Ehdm0yNDyCNE'
+    'token': 'MTA2MDY0Mzc0MzMzNTcxNDg3Ng.G_wCBk.IQimvwdwUpafLsfgEMSwgKpBfPFiSRt0z2Dyto'
 }
 urls = {
     'mamamia': 'https://cdn.discordapp.com/attachments/698200479217483860/1041419679903784960/telegram-cloud-photo-size-2-5442857354250011359-x.jpg',
-    'bolid': 'https://cdn.discordapp.com/attachments/698200479217483860/1041414868420534352/PgCb6Si_c4t5HgY7Jn_KtexcWJtGR8TbI9vhpFEwIauGDJ1Xrftrylfl6bbl7qHh-wwUn7SUttIWGZuAT08ArR7m.jpg'
+    'bolid': 'https://cdn.discordapp.com/attachments/698200479217483860/1041414868420534352/PgCb6Si_c4t5HgY7Jn_KtexcWJtGR8TbI9vhpFEwIauGDJ1Xrftrylfl6bbl7qHh-wwUn7SUttIWGZuAT08ArR7m.jpg',
+    'horner': 'https://sun9-west.userapi.com/sun9-8/s/v1/ig2/DQ7aiQawh3uMZwZ_xCCPlKVFDUYujPhiCWI_3jriwsw8aI2ihriyd4wrfjvy6jRJO1mc6DRsH36DuL-_W5rX-tMw.jpg?size=1280x905&quality=96&type=album',
+    'auf': 'https://sun9-east.userapi.com/sun9-75/s/v1/ig2/Dls8y6O_necDm8Y8c3Nd6CeHiwtARJsr9yKUseZB3ZTbj_pY7cS2aRFCib86HbU4xVrFdJJKxeVqg98fp_aFKfzg.jpg?size=1600x900&quality=96&type=album',
+    'lechair': 'https://sun9-west.userapi.com/sun9-10/s/v1/ig2/1lNF6bHl-H0A4kykn8fs3nCIjRotu4wkbdoBxQuOCLHTXma3NudK6qmCGW2RccFPqyw7RbE4QUVlN84xwCPYGhDf.jpg?size=714x745&quality=95&type=album',
+    'leave': 'https://sun9-west.userapi.com/sun9-53/s/v1/ig2/_IYaCOlujLJpKwpJGhSYOvu2KTMPfmB4vWhs8mRMX2zIwv6c-2IvCHLHoHbhDQdACt95f7_Nul6xG9bD7TU3c_eV.jpg?size=720x828&quality=96&type=album',
+    'mazepin': 'https://sun9-west.userapi.com/sun9-16/s/v1/ig2/0Y6g9wITSxdr8ImbLQISs7rq7KEqHJNiRGSlwI0Ayol6WibowuKklzEPotxffhGNMTfUpqOXcPaWsNUi83lUHIKF.jpg?size=1431x1125&quality=96&type=album',
+    'classic_ferrari': 'https://media.tenor.com/_yPGWdDANegAAAAd/ferrari-the-boys.gif',
+    'kind_gasly': 'https://sun9-east.userapi.com/sun9-32/s/v1/ig2/VSmMnFpXw_KcEWOm5mngPEFXN45TmMFRhumghPTZ0ZBQnCzfzcbzhPsO3TN-1aG4Dwh1JhSUOQtjxAKp4nuJH3HB.jpg?size=330x440&quality=96&type=album',
+    'piston': 'https://sun9-north.userapi.com/sun9-84/s/v1/ig2/SV4mRJLf4oCn70Nd4SJFVCZWLRFRulWLDCgExAjuDIGgGI0J1OR9qgua3jvhSD9dTglk4y3xA9LfE53i06Tk6yfF.jpg?size=1000x981&quality=95&type=album',
+    'genshin': 'https://sun9-east.userapi.com/sun9-73/s/v1/ig2/j9f0_27OT3osimSt-Wb0ER1QMzCdHu415hPAyvpb7IMhw0yMtn7n4zqe_9LLrfZJUVp9Oejlnce947ABq2XtZgf-.jpg?size=2541x1426&quality=95&type=album',
+    'max': 'https://sun9-east.userapi.com/sun9-20/s/v1/ig2/1WusdHoH5xNN5xvh0KvOG1-NspqRH6KOgFE47MJMqVmakCTNU1v2ODqsQwh16nmbA5Lde3YDHvzQiN72du565DQw.jpg?size=960x720&quality=96&type=album',
+    'goatifi': 'https://pbs.twimg.com/media/FiAzGdsWIAAkCuH.jpg',
+    'beer': 'https://sun9-west.userapi.com/sun9-11/s/v1/ig2/kjOL_lP51MzIP04aSArn9LZ-Q83jOEx4NO1L2ZtEcuFb3eC_sUuZHuBbN-cwbhWl7b-g77WDOJhlWrAfGbyoYJnW.jpg?size=714x880&quality=95&type=album',
+    'clowns': 'https://sun9-west.userapi.com/sun9-2/s/v1/ig2/MVxgEcX0lt2ilRV_MGFC4z_60sJvCfPwCYK6z_RVs61c3Bie8rP13G7uQoglmJALOuYaNFDi9bG9VkIvWv852ti-.jpg?size=640x426&quality=95&type=album',
+    'strategy': 'https://sun9-east.userapi.com/sun9-33/s/v1/ig2/rcY4xDxYS9W5uBYEQvVGXrIdWAtUrZqtWe7tU_17kSYbHy7ZtRvf1hkbj6XMPSuhhZ56eqxyoQKkZVRKnMaGBw9M.jpg?size=919x643&quality=95&type=album'
+}
+colors = {
+    'redbull': 0x223971,
+    'ferrari': 0xff2800,
+    'mercedes': 0x00d2be,
+    'alpine': 0x0090ff,
+    'haas': 0xffffff,
+    'astonmartin': 0x006f62,
+    'alphatauri': 0x2b4562,
+    'mclaren': 0xff8700,
+    'alfaromeo': 0x900000,
+    'williams': 0x005aff,
+    'f1': 0xff1801
 }
